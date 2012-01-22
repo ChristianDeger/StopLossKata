b1add5c4efd7338ca61b30c13ae9e566cccc1440
Christian Deger <christian@deger.eu>
2012-01-22 19:57:28 +0100

    First spec

---
+++ StopLossKata/StopLossSpecs.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace StopLossKata
+{
+    [Subject(typeof (StopLossStock))]
+    public class When_buying_stop_loss_stock_without_price_change
+    {
+        Because of = () => { stock = new StopLossStock(10, 1, () => stockLossTriggered = true); };
+
+        It should_not_trigger_stock_loss = () => stockLossTriggered.ShouldBeFalse();
+
+        static StopLossStock stock;
+        static bool stockLossTriggered;
+    }
+}
---
+++ StopLossKata/StopLossStock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StopLossKata
+{
+    public class StopLossStock
+    {
+        public StopLossStock(int price, int limit, Action stockLossTrigger)
+        {
+        }
+    }
+}
