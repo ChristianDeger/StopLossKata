b4eb40f461d830002528b28e918650769005848a
Christian Deger <christian@deger.eu>
2012-01-24 08:01:45 +0100

    Trailing stop loss with timeout

--- StopLossKata/StopLossSpecs.cs
+++ StopLossKata/StopLossSpecs.cs
@@ -129,13 +129,37 @@ namespace StopLossKata
     }
 
     [Subject(typeof(StopLossStock))]
-    public class When_buying_stop_loss_stock_and_price_raises_and_drops_below_new_trailing_limit_for_30_seconds
+    public class When_buying_stop_loss_stock_and_price_raises_for_14_seconds_and_drops_below_possible_new_trailing_limit_for_30_seconds
     {
         Establish context = () =>
         {
             bus = new FakeBus();
             stock = new StopLossStock(10, 1, bus);
             stock.Handle(new PriceChanged(11));
+            bus.TimewarpSeconds(14);
+        };
+
+        Because of = () =>
+        {
+            stock.Handle(new PriceChanged(9));
+            bus.TimewarpSeconds(30);
+        };
+
+        It should_not_trigger_stock_loss = () => bus.ShouldNotTriggerStopLoss();
+
+        static StopLossStock stock;
+        static FakeBus bus;
+    }
+
+    [Subject(typeof(StopLossStock))]
+    public class When_buying_stop_loss_stock_and_price_raises_for_15_seconds_and_drops_below_new_trailing_limit_for_30_seconds
+    {
+        Establish context = () =>
+        {
+            bus = new FakeBus();
+            stock = new StopLossStock(10, 1, bus);
+            stock.Handle(new PriceChanged(11));
+            bus.TimewarpSeconds(15);
         };
 
         Because of = () =>
--- StopLossKata/StopLossStock.cs
+++ StopLossKata/StopLossStock.cs
@@ -2,21 +2,23 @@
 {
     public class StopLossStock
     {
-        readonly int _limit;
+        readonly int _offset;
         readonly IBus _bus;
         int _currentPrice;
+        int _limit;
         bool _allowPriceDropTimeout;
 
-        public StopLossStock(int initialPrice, int limit, IBus bus)
+        public StopLossStock(int initialPrice, int offset, IBus bus)
         {
             _currentPrice = initialPrice;
-            _limit = limit;
+            _offset = offset;
+            _limit = _currentPrice - _offset;
             _bus = bus;
         }
 
         public void Handle(PriceChanged priceChanged)
         {
-            if (priceChanged.NewPrice < _currentPrice - _limit)
+            if (priceChanged.NewPrice < _limit)
             {
                 _bus.Publish(new Timeout(30, PriceDropTimeout));
                 _allowPriceDropTimeout = true;
@@ -26,6 +28,9 @@
                 _allowPriceDropTimeout = false;
             }
 
+            if (priceChanged.NewPrice > _currentPrice)
+                _bus.Publish(new Timeout(15, () => SetNewLimitTimout(priceChanged.NewPrice - _offset)));
+
             _currentPrice = priceChanged.NewPrice;
         }
 
@@ -33,6 +38,11 @@
         {
             if (_allowPriceDropTimeout)
                 _bus.Publish(new TriggerStockLoss());
+        }
+
+        private void SetNewLimitTimout(int limit)
+        {
+            _limit = limit;
         }
     }
 }
